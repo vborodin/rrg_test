369f38423f548f2bb1181efb97e71fb416297947
vladimir.borodin <vborodin@yandex.ru>
2019-11-17 14:39:58 +0700

    Menu creation
    - load planets from Resources/PlanetData
    - create button in menu for each planet

--- Assets/Scripts/MenuBootloader.cs
+++ Assets/Scripts/MenuBootloader.cs
@@ -1,6 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using RRGTest.Planets;
 
-public class MenuBootloader : MonoBehaviour
-{
-    
+namespace RRGTest {
+    public class MenuBootloader : MonoBehaviour {
+        #pragma warning disable 0649
+        [SerializeField]
+        private GameObject buttonPrefab;
+        [SerializeField]
+        private Transform contentHolder;
+        #pragma warning restore 0649
+
+        private void Start() {
+            var planets = Resources.LoadAll("PlanetData", typeof(PlanetData));
+            foreach (PlanetData p in planets) {
+                var b = Instantiate(buttonPrefab);
+                b.transform.SetParent(contentHolder, false);
+                b.GetComponentInChildren<Text>().text = p.planetName;
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/PlanetData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace RRGTest.Planets {
+    [CreateAssetMenu(fileName = "NewPlanetData", menuName = "Planet Data")]
+    public class PlanetData : ScriptableObject {
+        [SerializeField]
+        public string planetName = "Default";
+    }
+}
